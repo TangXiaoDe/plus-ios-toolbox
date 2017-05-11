62e5a9309fa97b8d97825e5d0aaf2c21020d5b97
GorCat <745318341@qq.com>
2017-05-11 17:26:33 +0800

    增加了 TSDate 的测试文件类

---
+++ Example/Tests/TestsDate.swift
@@ -0,0 +1,13 @@
+//
+//  TestsDate.swift
+//  AiLiToolbox
+//
+//  Created by GorCat on 2017/5/11.
+//  Copyright © 2017年 AiLi Toolbox. All rights reserved.
+//
+
+import UIKit
+
+class TestsDate: QuickSpec {
+
+}

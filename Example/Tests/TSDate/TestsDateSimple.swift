ffa82c682392e5fd56a2f94373f129902e322e66
GorCat <745318341@qq.com>
2017-05-12 11:00:35 +0800

    增加了 TSDate 的 simple 的测试

--- Example/Tests/TSDate/TestsDateSimple.swift
+++ Example/Tests/TSDate/TestsDateSimple.swift
@@ -14,12 +14,25 @@ import AiLiToolbox
 class TestsDateSimple: TestsDate {
 
     override func spec() {
-        describe("传入一个时间戳") {
-            let numberStringArray = "1,2,3"
+        resetTimes()
+        describe("传入一天之内的时间") {
+            let oneMinuteString = TSDate(now: now).dateString(.simple, nsDate: oneMinute as NSDate)
+            let oneHourString = TSDate(now: now).dateString(.simple, nsDate: oneHour as NSDate)
+            let oneDayString = TSDate(now: now).dateString(.simple, nsDate: oneDay as NSDate)
+            let twoDayString = TSDate(now: now).dateString(.simple, nsDate: twoDay as NSDate)
+            let nineDayString = TSDate(now: now).dateString(.simple, nsDate: nineDay as NSDate)
+            let nineDayMoreString = TSDate(now: now).dateString(.simple, nsDate: nineDayMore as NSDate)
             it("将时间戳转换成显示字符串") {
-                let numbers = [1, 2, 3]
-                let convertNumberStringArray = numbers.convertToString()
-                expect(convertNumberStringArray!) == numberStringArray
+                let today = "今\n天"
+                let yesterday = "昨\n天"
+                let nineDayAnswer = "09\n5 月"
+                let nineDayMoreAnswer = "01\n10月"
+                expect(oneMinuteString) == today
+                expect(oneHourString) == today
+                expect(oneDayString) == today
+                expect(twoDayString) == yesterday
+                expect(nineDayString) == nineDayAnswer
+                expect(nineDayMoreString) == nineDayMoreAnswer
             }
         }
     }

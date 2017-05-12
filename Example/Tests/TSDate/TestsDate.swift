4574540ea054601c422c30b82d218811f8953dd7
GorCat <745318341@qq.com>
2017-05-12 09:53:04 +0800

    设置了属性时间

--- Example/Tests/TSDate/TestsDate.swift
+++ Example/Tests/TSDate/TestsDate.swift
@@ -10,4 +10,24 @@ import Quick
 
 class TestsDate: QuickSpec {
 
+    /// 作为标准的时间
+    var now = Date()
+    /// 一分钟之内的时间
+    var oneMinute = Date()
+    /// 一小时之内的时间
+    var oneHour = Date()
+    /// 一天之内的时间
+    var oneDay = Date()
+    /// 1天到2天之内的时间
+    var twoDay = Date()
+    /// 2天到9天之内的时间
+    var nineDay = Date()
+    /// 9天以上的时间
+    var nineDayMore = Date()
+
+    /// 重置时间
+    func resetTimes() {
+        
+    }
+
 }

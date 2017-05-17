e10cdb6d8f2e8bdcccfa5c7ac0b3ee8c19ef8e93
LipYoung <447209844qq@gmail.com>
2017-05-17 10:46:01 +0800

    初始化了测试类

---
+++ Example/Tests/TestsRequestNetworkData.swift
@@ -0,0 +1,16 @@
+//
+//  TestsRequestNetworkData.swift
+//  AiLiToolbox
+//
+//  Created by lip on 2017/5/17.
+//  Copyright © 2017年 AiLi Toolbox. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Mockingjay
+import AiLiToolbox
+
+class TestsRequestNetworkDataSpec: QuickSpec {
+    
+}

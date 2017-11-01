7a54acfdb2b45714e24cd4cc9b941152e0b5939d
LipYoung <mainbundle@gmail.com>
2017-11-01 11:48:34 +0800

    增加了数据处理函数

--- AiLiToolbox/DataTransmission/DataProcess.swift
+++ AiLiToolbox/DataTransmission/DataProcess.swift
@@ -13,7 +13,26 @@ extension Array {
     ///
     /// - Note: 
     ///     - 例如:[1,2,3] -> "1,2,3"
-    ///     - 返回的字符串均以`,`分割,且末尾不带字符
+    ///     - 返回的字符串均以`,`分割,且末尾带字符
+    /// - Warning: 只允许在和服务器通讯时,使用此类字符串
+    public func convertToString() -> String? {
+        if self.isEmpty {
+            return nil
+        }
+        var tempArray: Array<String> = [String]()
+        for number in self {
+            tempArray.append("\(number)")
+        }
+        return tempArray.joined(separator: ",")
+    }
+}
+
+extension Set {
+    /// 将集合转为 字符串
+    ///
+    /// - Note:
+    ///     - 例如:[1,2,3] -> "1,2,3"
+    ///     - 返回的字符串均以`,`分割,且末尾带字符
     /// - Warning: 只允许在和服务器通讯时,使用此类字符串
     public func convertToString() -> String? {
         if self.isEmpty {
